fc228ffe67813be558b0b65dd27014a905fb9105
Vimal Daga <vimal.linuworld@gmail.com>
2021-02-28 13:23:16 +0530

    g

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Linux World!  <b>hello !!</b>  <img src='https://myimagest.blob.core.windows.net/myclwimage/vimal.jpg'/>"
+    return "Linux World!  <b>hello !!</b>  <img src='https://myimagest.blob.core.windows.net/myclwimage/vimal.jpg'  width='150' height='200' />"
